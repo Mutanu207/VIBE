9a83ddcc9e43d45799e327ad7df0525340c1d6b5
Mutanu207 <mutanumbuvi8@gmail.com>
2026-01-27 16:24:37 +0300

    added a button component and added the button to landing page,done with its ui

--- vibe-frontend/src/Pages/LandingPage.jsx
+++ vibe-frontend/src/Pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../Component/Navbar";
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import PrimaryButton from "../Component/PrimaryButton";
 function LandingPage(){
     return(
         <Box sx={{ minHeight: "100vh",
@@ -13,11 +14,16 @@ function LandingPage(){
                                 alignItems: 'center',
                                 display: 'flex' }}>
             <Container maxWidth="md">
-                <Typography component="h1" variant="h3"
+                <Typography component="h1" variant="h3" gutterBottom
                             sx={{ color: 'white', fontWeight: 'bold', textAlign: 'center', gutterBottom: true }}>
                     Music that matches your mood</Typography>
+                    <Box sx={{ display: "flex", justifyContent: "center",mt:4 }}>
+                    <PrimaryButton background="#8239f7ff" color="white" size="large">
+                        Get Recommendation
+                </PrimaryButton>
+                </Box>
             </Container>
-            </Box>
+          </Box>
         </Box>
     )
 }
