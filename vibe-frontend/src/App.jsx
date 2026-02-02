71b288aee2f1be98add1a5ef1832c8a6174ae2c4
Mutanu207 <mutanumbuvi8@gmail.com>
2026-02-02 14:47:58 +0300

    added the about page to the project

--- vibe-frontend/src/App.jsx
+++ vibe-frontend/src/App.jsx
@@ -3,12 +3,16 @@ import LandingPage from './Pages/LandingPage';
 import "./App.css";
 import Navbar from './Component/Navbar';
 import Box from '@mui/material/Box';
+import AboutPage from './Pages/AboutPage';
 function App() {
   return (
     <div>
       <Navbar />
       <Box id={'Home'}>
       <LandingPage />
+      </Box>
+      <Box id={'About'}>
+        <AboutPage />
       </Box>
     </div>
       
