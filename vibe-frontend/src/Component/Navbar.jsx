f2e4f71decb53210fc80b226f9e1c525dca50942
Mutanu207 <166437115+Mutanu207@users.noreply.github.com>
2026-02-09 15:57:28 +0300

    Update navigation items in Navbar component
    Removed 'How It Works' from the navigation items.

--- vibe-frontend/src/Component/Navbar.jsx
+++ vibe-frontend/src/Component/Navbar.jsx
@@ -16,7 +16,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'How It Works'];
+const navItems = ['Home', 'About'];
 
 function Navbar(props) {
   const { window } = props;
