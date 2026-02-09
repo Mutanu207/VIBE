8f496641d6fbdbc275beab5d594be481dcef8dfa
Mutanu207 <mutanumbuvi8@gmail.com>
2026-02-09 16:49:00 +0300

    sorted missing file issues

---
+++ vibe-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LandingPage from './Pages/LandingPage';
+import "./App.css";
+import Navbar from './Component/Navbar';
+import AboutPage from './Pages/AboutPage';
+import MoodPage from './Pages/MoodPage';
+import { Routes,Route } from 'react-router-dom';
+function App() {
+  return (
+        <>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/mood" element={<MoodPage />} />
+        </Routes>
+        </>
+  );
+}
+export default App; 
